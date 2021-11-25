372758c8bca79a060a2d95a9a1f6aab6f66db1f2
Abdul Basit <30389646+AbdulBasitAmin@users.noreply.github.com>
2021-11-25 10:41:04 +0500

    Inserting_Data_using_Ajax_and_Jquery

---
+++ Inserting_Data_using_Ajax/Inserting_Data_using_Ajax/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Inserting_Data_using_Ajax.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inserting_Data_using_Ajax.Controllers
+{
+    public class HomeController : Controller
+    {
+        ajax_testingEntities db = new ajax_testingEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(tbl_questions q)
+        {
+            db.tbl_questions.Add(q);
+            db.SaveChanges();
+
+
+
+
+            return View(q);
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Inserting_Data_using_Ajax/Inserting_Data_using_Ajax/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Inserting_Data_using_Ajax.MvcApplication" Language="C#" %>
